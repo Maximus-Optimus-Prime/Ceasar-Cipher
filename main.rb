7446572775723c1cd30e9197e4afd2e1d76acdd4
Maximus-Optimus-Prime <maximinbourgeais@gmail.com>
2024-09-02 17:40:15 +0200

    add Uppercase and non alphabetic letter

--- main.rb
+++ main.rb
@@ -2,15 +2,22 @@ def ceasar_cipher(string, shift)
   res = ""
   alphabet = ("a".."z").to_a
   string.each_char do |char|
-    i = alphabet.index(char)
-    index = i + shift > alphabet.length - 1 ? i + shift - alphabet.length : i + shift
-    cipher_letter = alphabet[index]
-    res += cipher_letter
+    uc = char == char.upcase
+    if alphabet.include?(char.downcase)
+      i = alphabet.index(char.downcase)
+      index = i + shift > alphabet.length - 1 ? i + shift - alphabet.length : i + shift
+      cipher_letter = alphabet[index]
+    else
+      cipher_letter = char
+    end
+    res += uc ? cipher_letter.upcase : cipher_letter
   end
   res
 end
 
 puts "word to cipher: "
 word = gets.chomp
-cipher_string = ceasar_cipher(word, 3)
+puts "shift number: "
+shift = gets.chomp
+cipher_string = ceasar_cipher(word, shift.to_i)
 puts "#{word} -> #{cipher_string}"
