7b73970b3f6d83dd7e2bb39005b5c58131104ce4
Maximus-Optimus-Prime <maximinbourgeais@gmail.com>
2024-09-03 11:16:39 +0200

    Final version

--- main.rb
+++ main.rb
@@ -15,9 +15,38 @@ def ceasar_cipher(string, shift)
   res
 end
 
-puts "word to cipher: "
-word = gets.chomp
-puts "shift number: "
-shift = gets.chomp
-cipher_string = ceasar_cipher(word, shift.to_i)
-puts "#{word} -> #{cipher_string}"
+def substrings(string, array)
+  res = Hash.new(0)
+  array.each do |word|
+    string.split.each do |string_split|
+      if string_split.downcase.include?(word.downcase)
+        res[word.downcase] += 1
+      end
+    end
+  end
+  res
+end
+
+### TEST ###
+
+def test_ceasar_cipher
+  puts "word to cipher: "
+  word = gets.chomp
+  puts "shift number: "
+  shift = gets.chomp
+  cipher_string = ceasar_cipher(word, shift.to_i)
+  puts "#{word} -> #{cipher_string}"
+end
+
+def test_substrings
+  dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+  print dictionary
+  puts
+  string = "Howdy partner, sit down! How's it going?"
+  puts string
+  puts substrings(string, dictionary)
+end
+
+### MAIN ###
+
+test_substrings
